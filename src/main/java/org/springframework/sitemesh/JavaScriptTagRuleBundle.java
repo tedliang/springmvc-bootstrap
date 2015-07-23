8d6fe49bdea01033ae151b3ac15c49b6684714a4
Ted Liang <teddyliang@gmail.com>
2015-07-23 21:13:43 +1000

    Support External JavaScriptTag.

--- src/main/java/org/springframework/sitemesh/JavaScriptTagRuleBundle.java
+++ src/main/java/org/springframework/sitemesh/JavaScriptTagRuleBundle.java
@@ -24,11 +24,14 @@ public class JavaScriptTagRuleBundle implements TagRuleBundle {
 
     private class JavaScriptTagsToContentRule extends ExportTagToContentRule {
 
+        private StringBuilder externalTags = new StringBuilder();
+
         private boolean hasContent = false;
 
         private final CharSequenceList contents = new CharSequenceList() {
             @Override
             public void writeTo(Appendable out) throws IOException {
+                out.append(externalTags);
                 if (hasContent) {
                     out.append("<script type=\"text/javascript\">");
                     super.writeTo(out);
@@ -40,6 +43,17 @@ public class JavaScriptTagRuleBundle implements TagRuleBundle {
         public JavaScriptTagsToContentRule(SiteMeshContext context, ContentProperty targetProperty, boolean includeInContent) {
             super(context, targetProperty, includeInContent);
             targetProperty.setValue(contents);
+        }
+
+        @Override
+        protected Object processStart(Tag tag) throws IOException {
+            if (tag.hasAttribute("src", true)) {
+                int length = externalTags.length();
+                tag.writeTo(externalTags);
+                // remove 'java', eg: <javascript ...> => <script ...>
+                externalTags.delete(length + 1, length + 5).append("</script>\n");
+            }
+            return super.processStart(tag);
         }
 
         @Override
