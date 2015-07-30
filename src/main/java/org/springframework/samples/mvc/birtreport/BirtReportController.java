d1a03612fe67126fc59ac9dda293c0c8f0e70cc8
Ted Liang <teddyliang@gmail.com>
2015-07-30 22:00:54 +1000

    BirtReport showcase.

---
+++ src/main/java/org/springframework/samples/mvc/birtreport/BirtReportController.java
@@ -0,0 +1,27 @@
+package org.springframework.samples.mvc.birtreport;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.inject.Inject;
+
+@Controller
+@RequestMapping("/birtreport")
+public class BirtReportController {
+
+    private BirtReportView birtReportView;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView testRequest(@RequestParam RenderType renderType) {
+        return birtReportView.loadTemplate(renderType,
+                "/WEB-INF/reports/ProductCatalog.rptdesign");
+    }
+
+    @Inject
+    public void setBirtReportView(BirtReportView birtReportView) {
+        this.birtReportView = birtReportView;
+    }
+}
