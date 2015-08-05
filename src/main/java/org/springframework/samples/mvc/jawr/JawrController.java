8f019b7a14a56363ca80b43c14fbf2f6ff446473
Ted Liang <teddyliang@gmail.com>
2015-08-05 20:54:14 +1000

    Jawr and Webjars showcases.

---
+++ src/main/java/org/springframework/samples/mvc/jawr/JawrController.java
@@ -0,0 +1,80 @@
+package org.springframework.samples.mvc.jawr;
+
+import net.jawr.web.context.ThreadLocalJawrContext;
+import net.jawr.web.servlet.JawrBinaryResourceRequestHandler;
+import net.jawr.web.servlet.JawrRequestHandler;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class JawrController implements ServletContextAware, InitializingBean, ServletContextListener {
+
+    private final static String CONFIG_LOCATION = "/jawr.properties";
+
+    private JawrRequestHandler jsHandler;
+
+    private JawrRequestHandler cssHandler;
+
+    private JawrRequestHandler binaryHandler;
+
+    private ServletContext context;
+
+    @RequestMapping("/**/*.js")
+    public void handleJs(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        jsHandler.processRequest(getPath(request), request, response);
+    }
+
+    @RequestMapping("/**/*.css")
+    public void handleCss(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        cssHandler.processRequest(getPath(request), request, response);
+    }
+
+    @RequestMapping("/**/*.{png|gif|jpeg|jpg|woff|eot|ttf}")
+    public void handleBinary(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        binaryHandler.processRequest(getPath(request), request, response);
+    }
+
+    private String getPath(HttpServletRequest request) {
+        return (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        jsHandler = new JawrRequestHandler(context, getInitParams("js"), null);
+        cssHandler = new JawrRequestHandler(context, getInitParams("css"), null);
+        binaryHandler = new JawrBinaryResourceRequestHandler(context, getInitParams("binary"), null);
+    }
+
+    private Map<String, Object> getInitParams(String type) {
+        Map<String, Object> initParams = new HashMap<>(2);
+        initParams.put("configLocation", CONFIG_LOCATION);
+        initParams.put("type", type);
+        return initParams;
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        ThreadLocalJawrContext.reset();
+    }
+
+    @Override
+    public void setServletContext(ServletContext context) {
+        this.context = context;
+    }
+
+}
--- src/main/webapp/WEB-INF/decorators/main-layout.jsp
+++ src/main/webapp/WEB-INF/decorators/main-layout.jsp
@@ -1,4 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://jawr.net/tags" prefix="jwr"%>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,10 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"
-        rel="stylesheet"  type="text/css" />
-  <link href="<c:url value="/resources/css/main.css" />"
-        rel="stylesheet"  type="text/css" />
+  <jwr:style src="/css/common.css"/>
   <sitemesh:write property='head'/>
   <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
   <!--[if lt IE 9]>
@@ -34,11 +32,7 @@
 
     <c:import url="/WEB-INF/views/tags/footer.jsp"/>
 
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="<c:url value="/resources/js/demo.js" />"></script>
-	<script type="text/javascript" src="<c:url value="/resources/js/json2.js" />"></script>
-    <script type="text/javascript" src="<c:url value="/resources/js/date.format.js" />"></script>
+    <jwr:script src="/js/common.js" />
     <sitemesh:write property='javascript'/>
 </body>
 </html>
